3c33616f23eedc051f42b8ad441f9a37ed4d0e1d
Peter Ellis <peter.ellis2013nz@gmail.com>
2020-11-28 19:47:32 +1100

    add event commentary

--- analysis/animate-map.R
+++ analysis/animate-map.R
@@ -1,6 +1,25 @@
 # create an animated map of the world with the location of Royal Navy ships
 # on each day.
 
+events <- tribble(~start_date, ~event,
+                 "1914-08-01", "Commencement of war",
+                 "1914-08-26", "First battle of Heligoland Bight",
+                 "1914-08-30", "",
+                 "1914-10-31", "Battle of Coronel",
+                 "1914-11-02", "Dardanelles campaign commences",
+                 "1914-11-11", "Chase and Battle of Falkland Islands",
+                 "1914-12-10", "",
+                 "1915-01-22", "Battle of Dogger Bank",
+                 "1915-01-26", "",
+                 "1916-05-29", "Battle of Jutland",
+                 "1916-06-03", "",
+                 "1917-11-16", "Second battle of Heligoland Bight",
+                 "1917-11-19", "",
+                 "1918-11-05", "Armistice",
+                 "1918-11-18", "Demobilisation in main theatres"
+                 ) %>%
+  mutate(start_date = as.Date(start_date))
+
 
 all_dates <- sort(unique(vessel_logs_sel$date))
 all_dates <- all_dates[all_dates >= "1914-01-01" & all_dates <= "1920-12-31"]
@@ -19,7 +38,10 @@ d2 <- vessel_logs_sel %>%
     grepl("cruiser", vessel_type) ~ "Other cruiser",
     TRUE ~ "Other"
   )) %>%
-  mutate(vessel_type_lumped = as.factor(vessel_type_lumped))
+  mutate(vessel_type_lumped = as.factor(vessel_type_lumped)) %>%
+  left_join(events, by = c("date" = "start_date")) %>%
+  arrange(date) %>%
+  fill(event, .direction = "down")
 
 pal <- brewer.pal(9, "Set1")
 names(pal) <- unique(d2$vessel_type_lumped)
@@ -36,8 +58,8 @@ for(i in 1:length(all_dates)){
     coord_sf() +
     theme_void(base_family = "Roboto") +
     scale_colour_manual(values = pal, labels = names(pal), drop = FALSE) +
-    labs(subtitle = "",
-         title = glue("Royal Navy ship locations 1914 to 1920: {format(the_date, '%d %B %Y')}"),
+    labs(subtitle = unique(d$event),
+         title = glue("Royal Navy ship locations 1914 to 1920:      {format(the_date, '%d %B %Y')}"),
          colour = "",
          caption = "Locations from log books compiled by naval-history.net; map by freerangestats.info") +
     theme(legend.position = "bottom",
